222771e8e0de8e71b634a514efe53f96d6322f6f
sanbgi <sanbgi@qq.com>
2022-03-17 11:39:49 +0800

    显示Opencl运行在哪个版本上

--- src/cpp/device/opencl/COpenCLDevice.cpp
+++ src/cpp/device/opencl/COpenCLDevice.cpp
@@ -142,12 +142,13 @@ protected://CObject
             cl::Platform plat;
             for (auto &p : platforms) {
                 std::string platver = p.getInfo<CL_PLATFORM_VERSION>();
-                std::cout << "opencl ver:" << platver << "\n"; 
+                std::cout << platver << "\n"; 
             }
             for (auto &p : platforms) {
                 std::string platver = p.getInfo<CL_PLATFORM_VERSION>();
                 if (platver.find("OpenCL 2.") != std::string::npos) {
                     plat = p;
+                    std::cout << "Run opencl on: " << platver;
                     break;
                 }
             }
